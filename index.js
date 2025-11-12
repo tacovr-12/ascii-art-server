70e076704b473088abd155762e195a34df0c4aea
tacovr-12 <thetacoman12@outlook.com>
2025-11-11 19:50:56 -0600

    update

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const files = {
 };
 
 const port = 8000;
-const lineDelay = 5;  // delay between each line
-const frameDelay = 105; // optional delay between frames
+const lineDelay = 1;  // delay between each line
+const frameDelay = 101; // optional delay between frames
 
 // Load frames from a file
 function loadFrames(filePath) {
