0ea799178d3e33eefe28097438f76d2bb4eb6fda
tacovr-12 <thetacoman12@outlook.com>
2025-11-11 18:08:08 -0600

    update

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import http from "http";
 import fs from "fs";
-import path from "path";
 
 const files = {
   "/nub-oil": "nub-oil.txt",
@@ -9,14 +8,37 @@ const files = {
   "/nub-67": "nub-67.txt"
 };
 
+const port = 8000;
+
 http.createServer((req, res) => {
-  const fileName = files[req.url];
-  if (fileName && fs.existsSync(fileName)) {
+  if (req.url === "/repeat") {
+    // Stream ASCII animation
+    if (!fs.existsSync("ball-ball-4.txt")) {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      return res.end("Missing ball-ball-4.txt\n");
+    }
+
+    const frames = fs.readFileSync("ball-ball-4.txt", "utf8").split("\n\n\n");
     res.writeHead(200, { "Content-Type": "text/plain" });
-    const fileStream = fs.createReadStream(fileName);
-    fileStream.pipe(res);
+
+    let i = 0;
+    const interval = setInterval(() => {
+      res.write("\x1b[2J\x1b[H"); // clear the screen
+      res.write(frames[i] + "\n");
+      i = (i + 1) % frames.length;
+    }, 100);
+
+    req.on("close", () => clearInterval(interval));
   } else {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("404: Not found\n");
+    // Handle static files
+    const fileName = files[req.url];
+    if (fileName && fs.existsSync(fileName)) {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      const fileStream = fs.createReadStream(fileName);
+      fileStream.pipe(res);
+    } else {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("404: Not found\n");
+    }
   }
-}).listen(8000, "0.0.0.0", () => console.log("ASCII server ready"));
+}).listen(port, "0.0.0.0", () => console.log("ASCII server ready"));
